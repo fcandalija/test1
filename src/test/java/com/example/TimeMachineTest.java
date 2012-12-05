d40ddf4a22d038ee61956575952792e35cfafaab
tiaped <pierre_struve@yahoo.fr>
2012-12-05 18:48:04 +0100

    Ensure that the test will be consistent

--- src/test/java/com/example/TimeMachineTest.java
+++ src/test/java/com/example/TimeMachineTest.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.util.Date;
 
-import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,18 +20,19 @@ public class TimeMachineTest {
     {
         final TimeMachine timeMachine = new TimeMachine();
         
-        final Date past = DateUtils.parseDate("2000-05-15", DATE_PATTERNS);
         final Date present = new Date();
-        final Date future = DateUtils.parseDate("2013-02-13", DATE_PATTERNS);
+        // ensures that past is past
+        final Date past = DateUtils.addDays(present, -1);
+        // ensures that future is future
+        final Date future = DateUtils.addDays(present, 1);
+        
+        // Sleep shortly to ensure that present is effectively in the past
+        Thread.sleep(10);
         
         Assert.assertTrue(timeMachine.isPast(past));
-        Assert.assertFalse(timeMachine.isPast(present));
+        // modified as in fact the present (defined little earlier) is already in the past :)
+        Assert.assertTrue(timeMachine.isPast(present));
         Assert.assertFalse(timeMachine.isPast(future));
     }
-    
-
-    private static final String[] DATE_PATTERNS = {
-        DateFormatUtils.ISO_DATE_FORMAT.getPattern()
-    };
-    
+        
 }
